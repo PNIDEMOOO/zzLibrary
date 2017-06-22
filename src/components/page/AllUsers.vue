26635225d095f4c904d69d0b0659ef216885efbf
PNIDEMOOO <452937660@qq.com>
2017-06-22 14:30:38 +0800

    管理员查询用户信息和记录

--- src/components/page/AllUsers.vue
+++ src/components/page/AllUsers.vue
@@ -3,41 +3,41 @@
         xxxxx
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-menu"></i>全部用户信息</el-breadcrumb-item>
+                <el-breadcrumb-item class="table-title"><i class="el-icon-menu table-title"></i> 全部用户信息</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <!--<div class="handle-box">-->
-            <!--<el-button class="handle-del mr10">批量删除</el-button>-->
-            <!--<el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">-->
-                <!--<el-option key="1" label="广东省" value="1"></el-option>-->
-                <!--<el-option key="2" label="湖南省" value="2"></el-option>-->
-            <!--</el-select>-->
-            <!--<el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>-->
-            <!--<el-button type="primary" icon="search">搜索</el-button>-->
+        <!--<el-button class="handle-del mr10">批量删除</el-button>-->
+        <!--<el-select v-model="select_cate" placeholder="筛选省份" class="handle-select mr10">-->
+        <!--<el-option key="1" label="广东省" value="1"></el-option>-->
+        <!--<el-option key="2" label="湖南省" value="2"></el-option>-->
+        <!--</el-select>-->
+        <!--<el-input v-model="select_word" placeholder="筛选关键词" class="handle-input mr10"></el-input>-->
+        <!--<el-button type="primary" icon="search">搜索</el-button>-->
         <!--</div>-->
         <!--<el-table :data="tableData" border style="width: 100%" ref="multipleTable" @selection-change="handleSelectionChange">-->
-            <!--<el-table-column type="selection" width="55"></el-table-column>-->
-            <!--<el-table-column prop="date" label="日期" sortable width="150">-->
-            <!--</el-table-column>-->
-            <!--<el-table-column prop="name" label="姓名" width="120">-->
-            <!--</el-table-column>-->
-            <!--<el-table-column prop="address" label="地址" :formatter="formatter">-->
-            <!--</el-table-column>-->
-            <!--<el-table-column label="操作" width="180">-->
-                <!--<template scope="scope">-->
-                    <!--<el-button size="small"-->
-                            <!--@click="handleEdit(scope.$index, scope.row)">编辑</el-button>-->
-                    <!--<el-button size="small" type="danger"-->
-                            <!--@click="handleDelete(scope.$index, scope.row)">删除</el-button>-->
-                <!--</template>-->
-            <!--</el-table-column>-->
+        <!--<el-table-column type="selection" width="55"></el-table-column>-->
+        <!--<el-table-column prop="date" label="日期" sortable width="150">-->
+        <!--</el-table-column>-->
+        <!--<el-table-column prop="name" label="姓名" width="120">-->
+        <!--</el-table-column>-->
+        <!--<el-table-column prop="address" label="地址" :formatter="formatter">-->
+        <!--</el-table-column>-->
+        <!--<el-table-column label="操作" width="180">-->
+        <!--<template scope="scope">-->
+        <!--<el-button size="small"-->
+        <!--@click="handleEdit(scope.$index, scope.row)">编辑</el-button>-->
+        <!--<el-button size="small" type="danger"-->
+        <!--@click="handleDelete(scope.$index, scope.row)">删除</el-button>-->
+        <!--</template>-->
+        <!--</el-table-column>-->
         <!--</el-table>-->
         <!--<div class="pagination">-->
-            <!--<el-pagination-->
-                    <!--@current-change ="handleCurrentChange"-->
-                    <!--layout="prev, pager, next"-->
-                    <!--:total="1000">-->
-            <!--</el-pagination>-->
+        <!--<el-pagination-->
+        <!--@current-change ="handleCurrentChange"-->
+        <!--layout="prev, pager, next"-->
+        <!--:total="1000">-->
+        <!--</el-pagination>-->
         <!--</div>-->
     </div>
 </template>
@@ -64,10 +64,11 @@
             },
             getData(){
                 let self = this;
-                if(process.env.NODE_ENV === 'development'){
+                if (process.env.NODE_ENV === 'development') {
                     self.url = '/ms/table/list';
-                };
-                self.$axios.post(self.url, {page:self.cur_page}).then((res) => {
+                }
+                ;
+                self.$axios.post(self.url, {page: self.cur_page}).then((res) => {
                     self.tableData = res.data.list;
                 })
             },
@@ -78,12 +79,12 @@
                 return row.tag === value;
             },
             handleEdit(index, row) {
-                this.$message('编辑第'+(index+1)+'行');
+                this.$message('编辑第' + (index + 1) + '行');
             },
             handleDelete(index, row) {
-                this.$message.error('删除第'+(index+1)+'行');
+                this.$message.error('删除第' + (index + 1) + '行');
             },
-            handleSelectionChange: function(val) {
+            handleSelectionChange: function (val) {
                 this.multipleSelection = val;
             }
         }
@@ -91,18 +92,29 @@
 </script>
 
 <style scoped>
-.handle-box{
-    margin-bottom: 20px;
-}
-.handle-del{
-    border-color: #bfcbd9;
-    color: #999;
-}
-.handle-select{
-    width: 120px;
-}
-.handle-input{
-    width: 300px;
-    display: inline-block;
-}
+    .crumbs {
+        margin: 10px 0;
+    }
+
+    .table-title {
+        font-size: 20px;
+    }
+
+    .handle-box {
+        margin-bottom: 20px;
+    }
+
+    .handle-del {
+        border-color: #bfcbd9;
+        color: #999;
+    }
+
+    .handle-select {
+        width: 120px;
+    }
+
+    .handle-input {
+        width: 300px;
+        display: inline-block;
+    }
 </style>
--- src/components/page/UserRecord.vue
+++ src/components/page/UserRecord.vue
@@ -1,37 +1,46 @@
 <template>
     <div class="table">
-        hhhh
-        <p>{{admin}}</p>
-        <!--<div class="userInfo">-->
-            <!--<p>借书期限：<span>30天</span></p>-->
-            <!--<p>剩余可借：<span>{{this.userinfo.available}}</span>本</p>-->
-            <!--<p>过期本数：<span>{{this.userinfo.dated}}</span>本</p>-->
-        <!--</div>-->
-        <!--<div class="crumbs">-->
-            <!--<el-breadcrumb separator="/">-->
-                <!--<el-breadcrumb-item><i class="el-icon-menu"></i>您的借书记录</el-breadcrumb-item>-->
-            <!--</el-breadcrumb>-->
-        <!--</div>-->
-        <!--<el-table :data="this.userRecord" border style="width: 1052px" ref="multipleTable">-->
-            <!--&lt;!&ndash;<el-table-column type="selection" width="55"></el-table-column>&ndash;&gt;-->
-            <!--<el-table-column prop="book" label="书名" width="200">-->
-            <!--</el-table-column>-->
-            <!--<el-table-column prop="borrow_time" label="借书日期" sortable width="200">-->
-            <!--</el-table-column>-->
-            <!--<el-table-column prop="deadline" label="还书日期" sortable width="200">-->
-            <!--</el-table-column>-->
-            <!--<el-table-column prop="isclosed" label="是否归还" sortable width="150">-->
-            <!--</el-table-column>-->
-            <!--<el-table-column prop="renew" label="剩余可续借次数" width="200">-->
-            <!--</el-table-column>-->
-            <!--<el-table-column prop="isclosed" label="操作" width="100">-->
-                <!--<template scope="scope">-->
-                    <!--<el-button v-if="scope.row.isclosed=='否' && scope.row.renew !=0" size="small"-->
-                               <!--@click="handleEdit(scope.$index, scope.row)">续借-->
-                    <!--</el-button>-->
-                <!--</template>-->
-            <!--</el-table-column>-->
-        <!--</el-table>-->
+        <el-input v-model="username" type="text" placeholder="请输入用户名" style="width: 200px"></el-input>
+        <el-button type="primary" @click="search()">查询</el-button>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item class="table-title"><i class="el-icon-menu table-title"></i> 用户信息
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div v-if="userinfo" class="userInfo">
+            <p>借书期限：<span>30天</span></p>
+            <p>剩余可借：<span>{{userinfo.available}}</span>本</p>
+            <p>过期本数：<span>{{userinfo.dated}}</span>本</p>
+        </div>
+        <div v-if="!userinfo" class="userInfo">
+            <p>请输入正确的用户名进行查询</p>
+        </div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item class="table-title"><i class="el-icon-menu table-title"></i> 该用户的借书记录
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <el-table :data="this.userRecord" border style="width: 912px" ref="multipleTable">
+            <el-table-column prop="book" label="书名" width="200">
+            </el-table-column>
+            <el-table-column prop="borrow_time" label="借书日期" sortable width="180">
+            </el-table-column>
+            <el-table-column prop="deadline" label="还书日期" sortable width="180">
+            </el-table-column>
+            <el-table-column prop="isclosed" label="是否归还" sortable width="150">
+            </el-table-column>
+            <el-table-column prop="renew" label="可续借次数" width="100">
+            </el-table-column>
+            <el-table-column prop="isclosed" label="操作" width="100">
+                <template scope="scope">
+                    <el-button v-if="scope.row.isclosed=='否' && scope.row.renew !=0" size="small"
+                               @click="handleEdit(scope.$index, scope.row)">续借
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
     </div>
 </template>
 
@@ -41,28 +50,37 @@
         props: ['admin'],
         data() {
             return {
-                User:null,
+                username: '',
+                userinfo: null,
+                userRecord: []
             }
         },
         created(){
-//            this.getData();
-            for (var i = 0; i < this.userRecord.length; i++) {
-                if (this.userRecord[i].isclosed) {
-                    this.userRecord[i].isclosed = "是"
-                }
-                else {
-                    this.userRecord[i].isclosed = "否"
-                }
-                console.log(this.userRecord)
-            }
         },
         methods: {
+            search() {
+                this.$http.get(urlconf.GetUserInfo(this.admin.token, this.username)).then(resp => {
+                    this.userinfo = resp.body
+                }, resp => {
+                    this.userinfo = null
+                })
+                this.$http.get(urlconf.GetUserRecord(this.admin.token, this.username)).then(resp => {
+                    this.userRecord = resp.body
+                    for (var i = 0; i < this.userRecord.length; i++) {
+                        this.userRecord[i].isclosed = this.userRecord[i].isclosed ? "是" : "否"
+                        this.userRecord[i].borrow_time = this.userRecord[i].borrow_time.replace('T', ' ')
+                        this.userRecord[i].deadline = this.userRecord[i].deadline.replace('T', ' ')
+                    }
+                }, resp => {
+                    this.userRecord = []
+                })
+            },
             handleEdit(index, row) {
-                this.$http.post(urlconf.Renew(this.user.token), {
-                    username: this.user.user,
+                this.$http.post(urlconf.Renew(this.admin.token), {
+                    username: this.username,
                     copy: row.copy
                 }).then(resp => {
-                    location.href=''
+                    location.href = ''
                 })
             }
         }
@@ -71,9 +89,18 @@
 
 <style scoped>
     .userInfo {
+        margin-top: 20px;
         border-left: 6px solid #eee;
         padding: 10px;
         margin-bottom: 20px;
+    }
+
+    .crumbs {
+        margin: 10px 0;
+    }
+
+    .table-title {
+        font-size: 20px;
     }
 
     .handle-box {
